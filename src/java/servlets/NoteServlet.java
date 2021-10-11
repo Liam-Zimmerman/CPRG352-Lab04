89afff5922f3a970fb378851a414618a911c3aef
854292 <854292@SAIT227683IT.ACDM.DS.SAIT.CA>
2021-10-11 01:32:00 -0600

    Revert "Finished Functionality Finished note keeper functionality."
    This reverts commit d3a014755d84af4ed0e93bdf43e0ba35b8bebe3f.

--- src/java/servlets/NoteServlet.java
+++ src/java/servlets/NoteServlet.java
@@ -24,8 +24,7 @@ public class NoteServlet extends HttpServlet {
         note.setContent(br.readLine());
         
         request.setAttribute("title", note.getTitle());
-        request.setAttribute("contents", note.getContent());
-       
+             
         if (request.getParameter("edit") == null) {
             
             getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
@@ -43,26 +42,7 @@ public class NoteServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
-        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path, false))); 
-        
-        String newTitle = request.getParameter("editTitle");
-        String newContent = request.getParameter("editContents");
-        
-        pw.println(newTitle);
-        pw.print(newContent);
-        pw.close();
-        
-        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
-        note.setTitle(br.readLine());
-        note.setContent(br.readLine());
-        
-        request.setAttribute("title", note.getTitle());
-        request.setAttribute("contents", note.getContent());
-        
-        getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
-        return;   
+          
     }
 
 }
